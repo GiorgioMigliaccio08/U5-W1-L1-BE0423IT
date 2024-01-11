853eda227a8c72748e405682881a8488bea8eb2e
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-11 16:54:42 +0100

    Commit 14

--- src/main/java/U5W1L1/demo/entities/Table.java
+++ src/main/java/U5W1L1/demo/entities/Table.java
@@ -11,6 +11,38 @@ public class Table {
     private boolean isFree;
     private double costoCoperto;
 
+    public int getNumTable() {
+        return numTable;
+    }
+
+    public void setNumTable(int numTable) {
+        this.numTable = numTable;
+    }
+
+    public int getNumMaxCoperti() {
+        return numMaxCoperti;
+    }
+
+    public void setNumMaxCoperti(int numMaxCoperti) {
+        this.numMaxCoperti = numMaxCoperti;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+
+    public double getCostoCoperto() {
+        return costoCoperto;
+    }
+
+    public void setCostoCoperto(double costoCoperto) {
+        this.costoCoperto = costoCoperto;
+    }
+
     public void print() {
         System.out.println("numero tavolo--> " + numTable);
         System.out.println("numero massimo coperti--> " + numMaxCoperti);
